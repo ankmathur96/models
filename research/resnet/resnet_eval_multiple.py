00f70679cb6e3785532751665c6cfe32bb007802
Ankit Mathur <ankitmathur@berkeley.edu>
2018-05-07 01:22:22 -0700

    evaluate batches 1 time

--- research/resnet/resnet_eval_multiple.py
+++ research/resnet/resnet_eval_multiple.py
@@ -17,31 +17,19 @@
 """
 from __future__ import print_function, division
 import time
-import six
-import sys
 import json
 
 import cifar_input
-import numpy as np
 import resnet_model
 import tensorflow as tf
-from tensorflow.contrib.framework.python.framework import checkpoint_utils
+
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('dataset', 'cifar10', 'cifar10 or cifar100.')
-tf.app.flags.DEFINE_string('mode', 'train', 'train or eval.')
-tf.app.flags.DEFINE_string('train_data_path', '',
-                           'Filepattern for training data.')
 tf.app.flags.DEFINE_string('eval_data_path', '',
                            'Filepattern for eval data')
 tf.app.flags.DEFINE_integer('image_size', 32, 'Image side length.')
-tf.app.flags.DEFINE_string('train_dir', '',
-                           'Directory to keep training outputs.')
 tf.app.flags.DEFINE_string('eval_dir', '',
                            'Directory to keep eval outputs.')
-tf.app.flags.DEFINE_integer('eval_batch_count', 50,
-                            'Number of batches to eval.')
-tf.app.flags.DEFINE_bool('eval_once', False,
-                         'Whether evaluate the model only once.')
 tf.app.flags.DEFINE_string('log_root1', '',
                            'Directory to keep the checkpoints. Should be a '
                            'parent directory of FLAGS.train_dir/eval_dir.')
@@ -51,81 +39,6 @@ tf.app.flags.DEFINE_string('m2name', 'm2', 'Model2 Name')
 tf.app.flags.DEFINE_integer('num_gpus', 0,
                             'Number of gpus used for training. (0 or 1)')
 tf.app.flags.DEFINE_integer('n_trials', 200, 'Number of trials for performance testing.')
-
-
-def train(n_classes):
-  """Training loop."""
-  hps = resnet_model.HParams(batch_size=128, num_classes=n_classes,
-                             min_lrn_rate=0.0001,
-                             lrn_rate=0.1,
-                             num_residual_units=5,
-                             use_bottleneck=False,
-                             weight_decay_rate=0.0002,
-                             relu_leakiness=0.1,
-                             optimizer='mom')
-  images, labels = cifar_input.build_input(
-      FLAGS.dataset, FLAGS.train_data_path, hps.batch_size, FLAGS.mode)
-  model = resnet_model.ResNet(hps, images, labels, FLAGS.mode)
-  model.build_graph()
-
-  param_stats = tf.contrib.tfprof.model_analyzer.print_model_analysis(
-      tf.get_default_graph(),
-      tfprof_options=tf.contrib.tfprof.model_analyzer.
-          TRAINABLE_VARS_PARAMS_STAT_OPTIONS)
-  sys.stdout.write('total_params: %d\n' % param_stats.total_parameters)
-
-  tf.contrib.tfprof.model_analyzer.print_model_analysis(
-      tf.get_default_graph(),
-      tfprof_options=tf.contrib.tfprof.model_analyzer.FLOAT_OPS_OPTIONS)
-
-  truth = tf.argmax(model.labels, axis=1)
-  predictions = tf.argmax(model.predictions, axis=1)
-  precision = tf.reduce_mean(tf.to_float(tf.equal(predictions, truth)))
-
-  summary_hook = tf.train.SummarySaverHook(
-      save_steps=100,
-      output_dir=FLAGS.train_dir,
-      summary_op=tf.summary.merge([model.summaries,
-                                   tf.summary.scalar('Precision', precision)]))
-
-  logging_hook = tf.train.LoggingTensorHook(
-      tensors={'step': model.global_step,
-               'loss': model.cost,
-               'precision': precision},
-      every_n_iter=100)
-
-  class _LearningRateSetterHook(tf.train.SessionRunHook):
-    """Sets learning_rate based on global step."""
-
-    def begin(self):
-      self._lrn_rate = 0.1
-
-    def before_run(self, run_context):
-      return tf.train.SessionRunArgs(
-          model.global_step,  # Asks for global step value.
-          feed_dict={model.lrn_rate: self._lrn_rate})  # Sets learning rate
-
-    def after_run(self, run_context, run_values):
-      train_step = run_values.results
-      if train_step < 40000:
-        self._lrn_rate = 0.1
-      elif train_step < 60000:
-        self._lrn_rate = 0.01
-      elif train_step < 80000:
-        self._lrn_rate = 0.001
-      else:
-        self._lrn_rate = 0.0001
-
-  with tf.train.MonitoredTrainingSession(
-      checkpoint_dir=FLAGS.log_root,
-      hooks=[logging_hook, _LearningRateSetterHook()],
-      chief_only_hooks=[summary_hook],
-      # Since we provide a SummarySaverHook, we need to disable default
-      # SummarySaverHook. To do that we set save_summaries_steps to 0.
-      save_summaries_steps=0,
-      config=tf.ConfigProto(allow_soft_placement=True)) as mon_sess:
-    while not mon_sess.should_stop():
-      mon_sess.run(model.train_op)
 
 def evaluate(n_classes):
   """Eval loop."""
@@ -171,15 +84,12 @@ def evaluate(n_classes):
   sess2.run(tf.initialize_all_variables())
   saver2.restore(sess2, ckpt_state2.model_checkpoint_path)
   for i in range(FLAGS.n_trials):
-    for _ in six.moves.range(FLAGS.eval_batch_count):
-      start = time.time()
-      predictions = sess1.run([model1.cost, model1.predictions, model1.labels])
-      timed_results['m1_latency'].append(time.time() - start)
-
-    for _ in six.moves.range(FLAGS.eval_batch_count):
-      start = time.time()
-      predictions = sess2.run([model2.cost, model2.predictions, model2.labels])
-      timed_results['m2_latency'].append(time.time() - start)
+    start = time.time()
+    predictions = sess1.run([model1.cost, model1.predictions, model1.labels])
+    timed_results['m1_latency'].append(time.time() - start)
+    start = time.time()
+    predictions = sess2.run([model2.cost, model2.predictions, model2.labels])
+    timed_results['m2_latency'].append(time.time() - start)
     print(str(i), str(timed_results['m1_latency'][-1]), str(timed_results['m2_latency'][-1]))
   with open('combined_latency.json', 'w') as out_f:
     json.dump(timed_results, out_f)
@@ -198,10 +108,7 @@ def main(_):
     num_classes = 100
 
   with tf.device(dev):
-    if FLAGS.mode == 'train':
-      train(num_classes)
-    elif FLAGS.mode == 'eval':
-      evaluate(num_classes)
+    evaluate(num_classes)
 
 
 if __name__ == '__main__':
